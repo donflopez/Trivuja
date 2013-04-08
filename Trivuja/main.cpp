cf2ba8988e8166c05acaeb96647ede1e4ba567af
Francisco López <donflopez@gmail.com>
2013-04-08 18:01:47 +0200

    Initial Commit

---
+++ Trivuja/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Trivuja
+//
+//  Created by Francisco López Liébana on 08/04/13.
+//  Copyright (c) 2013 Francisco López Liébana. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
